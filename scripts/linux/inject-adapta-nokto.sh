e586ef636d1a481b8ed82a302f787d9b355ae183
Sefa Eyeoglu <contact@scrumplex.net>
2018-02-10 18:50:37 +0100

    Updated STYLESHEET_SOURCE to reflect github changes faster

--- scripts/linux/inject-adapta-nokto.sh
+++ scripts/linux/inject-adapta-nokto.sh
@@ -15,7 +15,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-STYLESHEET_SOURCE="https://cdn.rawgit.com/Scrumplex/Discord-Adpata-Nokto/master/stylesheet/adapta-nokto.css"
+STYLESHEET_SOURCE="https://rawgit.com/Scrumplex/Discord-Adpata-Nokto/master/stylesheet/adapta-nokto.css"
 
 set -e
 
