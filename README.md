9996374ae2ea3a26a58b6b7d5b9de3979e705537
Sefa Eyeoglu <contact@scrumplex.net>
2018-02-10 17:39:12 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Discord-Adapta-Nokto
+A theme patch for Discord on Linux.
+
+# Support
+Currently it is unknown if this theme-patch will work after an update.
+
+For now this patch officially supports Discord 0.0.4 on Linux.
+
+# Installation
+Download and run the script [inject-adapta-nokto.sh](scripts/linux/inject-adapta-nokto.sh).
+
+It will require a recent version of npm, because it needs to modify Electron specific files.
+
+It will install the npm package [asar](https://github.com/electron/asar) gloablly in order to extract and pack files.
+
+# License
+This project is licensed under the GNU General Public License v3. You can find more information about it in the [LICENSE](LICENSE) file.
---
+++ scripts/linux/inject-adapta-nokto.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#    Discord-Adapta-Nokto
+#    Copyright (C) 2018 Sefa Eyeoglu <contact@scrumplex.net> (https://scrumplex.net)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+set -e
+sudo npm install -g asar
+
+cd ~/.config/discord/0.0.4/modules/discord_desktop_core
+
+cp core.asar core.before-adapta-nokto-patch.asar
+
+TMP=$(mktemp -d)
+asar e core.asar $TMP
+sed -i "121i _mainWindow\$webConten.executeJavaScript('var elem=document.createElement(\"link\");elem.setAttribute(\"href\",\"https://cdn.scrumplex.net/discord-adapta-nokto.css\"),elem.setAttribute(\"rel\",\"stylesheet\"),document.head.appendChild(elem);');" $TMP/app/mainScreen.js
+asar p $TMP core.asar
